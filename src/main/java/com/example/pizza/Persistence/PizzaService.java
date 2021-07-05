d7c90562d8c2f35d75da0943c7df35ee67e504f2
Marlon Pizarro <96xanderp@gmail.com>
2021-07-05 15:50:16 -0500

    Endpoint pizza consulta pizza topping hot

--- src/main/java/com/example/pizza/Persistence/PizzaService.java
+++ src/main/java/com/example/pizza/Persistence/PizzaService.java
@@ -1,6 +1,10 @@
 package com.example.pizza.Persistence;
 
 
+import com.example.pizza.Conexion.OpenOWL;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +13,33 @@ public class PizzaService implements ServicioPizza{
 
     @Override
     public List<String> listar() {
-        return ListPizzas;
+        try {
+            String queryString;
+            queryString = "PREFIX pizza:<http://www.semanticweb.org/marlonl/ontologies/2021/PizzaTutorial#>"
+                    + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
+                    + "PREFIX owl: <http://www.w3.org/2002/07/owl#>"
+                    + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
+                    + "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>"
+                    + "SELECT DISTINCT ?s "
+                    + "WHERE {  ?s rdfs:subClassOf ?restriction . " +
+                    " ?restriction owl:onProperty pizza:hasSpiciness . " +
+                    " ?restriction owl:hasValue pizza:Hot .}";
+
+            ResultSet results = OpenOWL.ExecSparQl(queryString);
+//            String results = OpenOWL.ExecSparQlString(queryString);
+            while (results.hasNext()) {
+                QuerySolution soln = results.nextSolution();
+                String NameOfPerson = soln.getResource("s").getLocalName();
+//                 Literal NameOfPerson = soln.getLiteral("Pizza");
+                if (NameOfPerson != null){
+                    ListPizzas.add(NameOfPerson.toString());
+                }
+            }
+            return ListPizzas;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println("entra");
+        }
+        return null;
     }
 }
--- src/main/java/com/example/pizza/controller/Controlador.java
+++ src/main/java/com/example/pizza/controller/Controlador.java
@@ -1,5 +1,8 @@
 package com.example.pizza.controller;
 
+import com.example.pizza.Persistence.PizzaService;
+import com.example.pizza.Persistence.ServicioPizza;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,17 +12,10 @@ import java.util.List;
 @RestController
 public class Controlador {
     List<String> lista= new ArrayList<>();
-//    @Autowired
-//    private ServicioPizza objServicio;
-
-    @GetMapping("/example")
-    public String listar(){
-//        ServicioPizza objServicioPizza = new PizzaService();
-//        lista=objServicioPizza.listar();
-//        for (int i = 0; i < lista.size(); i++) {
-//
-//            System.out.println(lista.get(i));
-//        }
-        return "Funciona";
+    @GetMapping("/listar")
+    public List<String> listar(){
+        ServicioPizza objServicioPizza = new PizzaService();
+        lista=objServicioPizza.listar();
+        return lista;
     }
 }
